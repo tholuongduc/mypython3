60d067ec57cbedaf46133e60d9c25e720cfc1634
tholuongduc <tholuongduc@live.com>
2021-08-10 20:21:08 +0700

    -Calculate the square of triangle

--- Draw_5_square.py
+++ Draw_5_square.py
@@ -1,15 +1,23 @@
 #Draw 5 square
 import turtle
+import random
 
+color = ['black', 'red', 'blue', 'green', 'pink']
+
+turtle.speed(10)
 turtle.pensize(3)
 turtle.right(90)
 for j in range (5):
+    choice_color = random.choice(color)
+    turtle.fillcolor(choice_color)
     for i in range(2):
+        turtle.begin_fill()
         turtle.forward(50)
         turtle.left(90)
         turtle.forward(100)
         turtle.left(90)
         turtle.forward(50)
+        turtle.end_fill()
     turtle.right(72)
 
 turtle.done()
