3e67dcf85ef5283e8bfec112c50baab3a938cc93
tholuongduc <tholuongduc@live.com>
2021-08-13 15:52:48 +0700

    Day6:Nhập/xuất dữ liệu, String, Import.

---
+++ Day6/Print_name_scores.py
@@ -0,0 +1,6 @@
+info = "{:15}\t{:9}\t{:8}\t{:14}"
+
+print(info.format("HO VA TEN","DIEM TOAN","DIEM VAN","DIEM NGOAI NGU"))
+print(info.format("Nguyen Van A", 10, 8, 10))
+print(info.format("Tran Van B", 9, 8, 9))
+print(info.format("Hoang Van C", 8, 8, 10))
