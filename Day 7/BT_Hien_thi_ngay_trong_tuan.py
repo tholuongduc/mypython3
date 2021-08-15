656699b339d78e7478f09608600b4869722e6935
tholuongduc <tholuongduc@live.com>
2021-08-15 21:06:04 +0700

    Day 7: BT Hien thi ngay trong tuan

---
+++ Day 7/BT_Hien_thi_ngay_trong_tuan.py
@@ -0,0 +1,22 @@
+conditionDay = False
+
+while (conditionDay == False):
+    day = int(input("Enter your number (from 1 to 7):"))
+    conditionDay = True
+    if day == 1:
+        print("Today is Monday")
+    elif day == 2:
+        print("Today is Tuesday")
+    elif day == 3:
+        print("Today is Wednesday")
+    elif day == 4:
+        print("Today is Thursday")
+    elif day == 5:
+        print("Today is Friday")
+    elif day == 6:
+        print("Today is Saturday")
+    elif day == 7:
+        print("Today is Sunday")
+    else:
+        print("Error, out of range! Please select again!")
+        conditionDay = False
