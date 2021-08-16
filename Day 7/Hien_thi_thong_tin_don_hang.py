c4b4fcbefd04bc91c70faa08a0aba788f8c3c633
tholuongduc <tholuongduc@live.com>
2021-08-16 20:13:19 +0700

    Day 7: BT Hien thi ngay trong tuan

--- Day 7/Hien_thi_thong_tin_don_hang.py
+++ Day 7/Hien_thi_thong_tin_don_hang.py
@@ -1,10 +1,10 @@
 money = int(input("Nhap so tien da mua:"))
 
 if money >= 150:
-    print("So tien phai tra:" + str(money - 15))
-elif money >= 100 and money < 150:
-    print("So tien phai tra:" + str(money - 25))
-elif money >= 150:
     print("So tien phai tra:" + str(money - 50))
+elif money >= 100:
+    print("So tien phai tra:" + str(money - 25))
+elif money >= 75:
+    print("So tien phai tra:" + str(money - 15))
 else:
     print("so tien phai tra:" + str(money))
