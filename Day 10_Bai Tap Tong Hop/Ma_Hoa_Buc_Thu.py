8b44ab856b80c58c1dbcd91b2a39e1263d0ac363
tholuongduc <tholuongduc@live.com>
2021-08-22 16:57:30 +0700

    Day 10: Bài tập mã hóa bức thư

--- Day 10_Bai Tap Tong Hop/Ma_Hoa_Buc_Thu.py
+++ Day 10_Bai Tap Tong Hop/Ma_Hoa_Buc_Thu.py
@@ -1,3 +1,15 @@
+#Vị Giám đốc công ty XYZ cần gửi một văn bản quan trọng tới đối tác. Để bảo mật văn bản, GĐ quyết định mã hóa văn bản trước khi gửi. Văn bản là một chuỗi S. Ông ta chia văn bản thành hai đoạn liên tiếp Sb và Se. Lần lượt viết hai chuỗi Sb và Se theo thứ tự ngược lại. Kết quả được chuỗi mã hóa Q. Để đọc được văn bản, chúng ta cần biết khóa để giải mã: đó là độ dài k của chuỗi Sb.
+#Yêu cầu: Viết chương trình mã hóa và giải mã một chuỗi nội dung.
+#Đầu vào
+#● Chọn chức năng: mã hóa hoặc giải mã (có thể chọn bằng số)
+#● Nhập vào chuỗi mã hóa Q (hoặc văn bản S) có độ dài từ 1 đến 1000
+#● Nhập vào khóa k
+#Kết quả:
+#● Nếu người dùng chọn chức năng mã hóa, hiển thị chuỗi mã hóa.
+#● Nếu người dùng chọn chức năng giải mã, hiển thị văn bản gốc.
+#Ví dụ: Nội dung bức thư S = 'programming' được chia thành 2 đoạn: Sb = 'program', Se = 'ming', nhận được xâu mã
+#hóa Q = ’margorpgnim’ với khóa k = 7.
+
 #Nhập đoạn văn bản
 s = input("Hãy nhập đoạn văn bản của bạn:")
 length_s = len(s)
