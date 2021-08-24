3c692df85d632bc1a98dafc4258ae1c09bbb929b
tholuongduc <tholuongduc@live.com>
2021-08-24 21:08:22 +0700

    Day 10: Bài tập xd chuoi doi xung

---
+++ Day 10_Bai Tap Tong Hop/BT_Chuoi_doi_xung.py
@@ -0,0 +1,6 @@
+chuoi = input("Nhập vào một chuỗi để kiểm tra: ")
+if chuoi == chuoi[::-1]:
+    print("* Chuỗi vừa nhập: ",chuoi)
+    print("* Chuỗi trên là chuỗi đối xứng")
+else: 
+    print("* Chuỗi trên Không đối xứng")
