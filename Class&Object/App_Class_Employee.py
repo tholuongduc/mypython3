5a789173b9fc48d37115f0b37355b5a4139117c8
tholuongduc <tholuongduc@live.com>
2021-09-25 21:48:41 +0700

    BT:Dictionary - Employee Clas

--- Class&Object/App_Class_Employee.py
+++ Class&Object/App_Class_Employee.py
@@ -4,7 +4,7 @@ employee01 = employee(ID="Tec01", First_name="Tho", Family_name="Luong Duc", Sal
 print(f'Employee Information:\n{employee01}')
 #Annual Salary
 annual_salary = employee01.get_annual_salary()
-print("Annual Salary: ",annual_salary)
+print("Annual Salary: ", annual_salary)
 #Salary increasement
 new_salary = employee01.incrase_salary(10)
 print("New salary: ", new_salary)
